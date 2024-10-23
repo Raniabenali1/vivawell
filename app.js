3bdac9c0fd7785fa4e1002682b41145bb8cd064b
Raniabenali1 <benalirania855@gmail.com>
2024-10-23 12:44:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const contactButton = document.querySelector('.contact-button');
+
+contactButton.addEventListener('click', () => {
+  alert('Thank you for contacting us! We will get back to you shortly.');
+});
