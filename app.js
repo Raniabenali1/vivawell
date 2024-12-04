7c5acc1b786a7fc63f549c25b43f684643470c00
Raniabenali1 <benalirania855@gmail.com>
2024-12-04 14:48:54 +0100

    aaa

--- app.js
+++ app.js
@@ -41,3 +41,31 @@ themeToggleButton.addEventListener('click', () => {
     themeIcon.classList.toggle('fa-sun', !isDarkMode);
     themeIcon.classList.toggle('fa-moon', isDarkMode);
 });
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(-${100 * currentSlide}%)`;
+  });
+  currentSlide = (currentSlide + 1) % slides.length;
+}, 3000);
+const animateCounter = (id, endValue) => {
+    let startValue = 0;
+    const duration = 2000; // 2 seconds
+    const increment = Math.ceil(endValue / (duration / 50));
+
+    const counter = setInterval(() => {
+        startValue += increment;
+        document.getElementById(id).innerText = startValue;
+
+        if (startValue >= endValue) {
+            document.getElementById(id).innerText = endValue;
+            clearInterval(counter);
+        }
+    }, 50);
+};
+
+animateCounter("livesImpacted", 10000);
+animateCounter("events", 150);
+animateCounter("volunteers", 300);
